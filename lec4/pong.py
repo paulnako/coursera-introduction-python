1e7ab28ec3fafe5ac85f8af1b8932c38a5ced841
vilion <takabatakekoichi@gmail.com>
2013-11-06 08:21:08 +0900

    pong in progress

--- lec4/pong.py
+++ lec4/pong.py
@@ -1,7 +1,6 @@
 # -*- coding:utf-8 -*-
 
 # Implementation of classic arcade game Pong
-
 import simplegui
 import random
 
@@ -25,6 +24,8 @@ def spawn_ball(direction):
     ball_pos = [WIDTH / 2,  HIGHT / 2]
     if direction == RIGHT:
         ball_vel = [1, 1]
+    elif direction == LEFT:
+        ball_vel = [-1, 1]
 
 # define event handlers
 def new_game():
@@ -33,7 +34,6 @@ def new_game():
 
 def draw(c):
     global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel
- 
         
     # draw mid line and gutters
     c.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
@@ -41,11 +41,15 @@ def draw(c):
     c.draw_line([WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1, "White")
         
     # update ball
-            
+    ball_pos = [ball_pos[0] + ball_vel[0],  ball_pos[1] + ball_vel[1] ]
+
     # draw ball
+    c.draw_circle( BALL_RADIUS, 1, "red", "white")       
     
     # update paddle's vertical position, keep paddle on the screen
-    
+    paddle1_pos = [paddle1_pos[0] + paddle1_vel[0], paddle1_pos[1] + paddle1_vel[1] ]  
+    paddle2_pos,
+
     # draw paddles
     
     # draw scores
