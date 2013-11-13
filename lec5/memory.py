5c9eee2862d5927f700f53ce527dd9b7dfa609ae
vilion <takabatakekoichi@gmail.com>
2013-11-14 08:18:52 +0900

    memory implementation is done.

--- lec5/memory.py
+++ lec5/memory.py
@@ -14,39 +14,43 @@ numbers = range(0, 8) + range(0, 8)
 random.shuffle(numbers)
 
 exposed = [ False ] * len(numbers)
+counter = 0
 
 # helper function to initialize globals
 def new_game():
-    pass  
+    global counter, exposed, numbers
+    exposed = [ False ] * len(numbers)
+    counter = 0
+    random.shuffle(numbers)
 
      
 # define event handlers
 def mouseclick(pos):
+    global one_previous, two_previous, state, counter
     # add game state logic here
     selected = pos[0] // 50
     if not exposed[ selected ]:
-        two_previous = one_previous
-        one_previous = selected
+        counter += 1
         if state == 0:
             state = 1
-            exposed[ pos[0] // 50 ] = True
+            exposed[ selected ] = True
         elif state == 1:
             state = 2
-            exposed[ pos[0] // 50 ] = True
+            exposed[ selected ] = True
         elif state == 2:
             if numbers[one_previous] != numbers[two_previous]:
                 exposed[one_previous] = False
-                exposed[one_previous] = False
-
-                     
+                exposed[two_previous] = False
+            exposed[ selected ] = True
             state = 1
-
+        two_previous = one_previous
+        one_previous = selected
     
                         
 # cards are logically 50x100 pixels in size    
 def draw(canvas):
+    global counter
     for i in range(0, 16):
-        canvas.draw_text(str(numbers[i]),( i * 50 + 5, 80), 80,  "black")
         canvas.draw_polygon( 
                             ( (0 + i * 50, 0), ( 0 + i * 50, 100 ), ( 50 + i * 50, 100 ), ( 50 + i * 50, 0 )) ,
                             1, 
@@ -54,7 +58,8 @@ def draw(canvas):
                             "green"
                             )
 
-    [ canvas.draw_text( str(numbers[i]), ( i * 50 + 5, 80), 80,  "black" ) for i in range( len( numbers ) ) if exposed[i] ]
+    [ canvas.draw_text( str(numbers[i]), ( i * 50 + 2, 80), 80,  "black" ) for i in range( len( numbers ) ) if exposed[i] ]
+    label.set_text("Turns = %d" % counter )
 
 
 # create frame and add a button and labels
